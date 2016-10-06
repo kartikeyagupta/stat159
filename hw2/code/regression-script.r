3330c8666373f51a7437798ca581c32d8a3271d8
kartikgupta <kartik.gupta@berkeley.edu>
2016-10-06 15:10:35 -0700

    Regression code updated

--- hw2/code/regression-script.r
+++ hw2/code/regression-script.r
@@ -1,4 +1,6 @@
+library("xtable")
 library(stargazer)
+library(testthat)
 
 ad = read.csv("./data/Advertising.csv", row.names = 1)
 
@@ -9,3 +11,10 @@ plot(ad$TV, ad$Sales, pch=16, col=rgb(255,0,0,150,maxColorValue=255), xlab = "TV
 abline(reg)
 
 stargazer(reg, type = "text", out = "./data/Tv~Sales-Reg.txt")
+
+xtable(reg)
+
+Quantity = c("Residual Standard Error","R2","F-Statistic")
+Value = c(as.numeric(sqrt(sum(as.vector(summary(reg)$residuals)^2)/(length(summary(reg)$residuals)-2))), as.numeric(summary(reg)$r.squared), as.numeric((summary(reg)$fstatistic)[1]))
+valuetable = data.frame(Quantity, Value)
+valuetable = xtable(valuetable)
