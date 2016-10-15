8fd61974e84283d83da6d13a927d76208c032d06
kartikgupta <kartik.gupta@berkeley.edu>
2016-10-14 19:49:46 -0700

    Regression-function.r - Added function description. Report.rmd - Added tables and plots.

--- hw3/code/functions/regression-functions.R
+++ hw3/code/functions/regression-functions.R
@@ -1,22 +1,36 @@
 
 
-
+#' @title Residual Sum of Sqaures - Returns RSS computed manually
+#' @param x - regression data (lm object)
+#' @return numeric RSS
 residual_sum_squares = function(x){
   as.numeric((sum(as.vector(summary(reg)$residuals)^2)))
 }
 
+#' @title Total Sum of Sqaures - Returns TSS computed manually
+#' @param x - regression data (lm object)
+#' @return numeric TSS
 total_sum_squares = function(x){
   as.numeric(sum((reg$y-mean(reg$y))^2))
 }
 
+#' @title R-Squared - Returns R^2 statistic computed manually
+#' @param x - regression data (lm object)
+#' @return numeric R2
 r_squared = function(x){
   as.numeric(1-(residual_sum_squares(x)/total_sum_squares(x)))
 }
 
+#' @title F-statistic - Returns F-stat computed manually
+#' @param x - regression data (lm object)
+#' @return numeric F-stat without degrees of freedom
 f_statistic = function(x){
   as.numeric(((total_sum_squares(x)-residual_sum_squares(x))/(length(coef(x))-1))/(residual_sum_squares(x)/(length(x$y)-length(coef(x))-1-1)))
 }
 
+#' @title Residual Standard Error - Returns RSE computed manually
+#' @param x - regression data (lm object)
+#' @return numeric RSE
 residual_std_error = function(x){
   as.numeric(sqrt((1/(length(x$y)-length(coef(x))-1-1))*residual_sum_squares(x)))
 }
